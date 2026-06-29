2c6d0944fdff8f5d32c970852def11119edf5caa
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-06-29 14:53:57 +0600

    naxt config mjs update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  images: {
+    remotePatterns: [
+      // Pixabay
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+      },
+
+      // imgbb
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+
+      // Any external image URL (KEEPED as you requested)
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
